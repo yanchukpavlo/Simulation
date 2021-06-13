f73e24aecd1a26861ef60dbcba4a8685747b5345
Pavlo Yanchuk <yanchukpavlo@gmail.com>
2021-06-13 15:34:26 +0300

    3
    Creating a script that provides the movement of agent.

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider), typeof(Rigidbody), typeof(MeshRenderer))]
+[RequireComponent(typeof(Animator))]
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Settings")]
+    [SerializeField] float forseMult = 200f;
+    [SerializeField] int startHP = 3;
+
+    int hp;
+
+    private void Start()
     {
-        
+        hp = startHP;
+        StartMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartMove()
     {
-        
+        GetComponent<Collider>().enabled = true;
+        GetComponent<Rigidbody>().AddForce(
+            new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)) * forseMult, 
+            ForceMode.Impulse);
+        GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+    }
+
+    void StopMove()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Animator>().SetTrigger("fadeIn");
+    }
+
+    void ActiveOff()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(gameObject.tag))
+        {
+            hp--;
+            if(hp <= 0)
+            {
+                StopMove();
+            }
+        }
     }
 }
